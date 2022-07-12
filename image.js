0407ae767ddd174852db62d0e9cdd368dac3f970
RaviGopinath <gopinathravi95@gmail.com>
2022-07-12 17:23:37 +0530

    g

--- image.js
+++ image.js
@@ -6,13 +6,17 @@ increaseButton.addEventListener("click", increase);
 
 function increase(){
     var currentWidth = image.width;
-    image.style.width = (currentWidth + 20) + 'px';
-
+    if(currentWidth < 700){
+        image.style.width = (currentWidth + 20) + 'px';
+        console.log(currentWidth);
+    }
 }
 
 decreaseButton.addEventListener("click" , decrease);
 
 function decrease(){
     var currentWidth = image.width;
+    if(currentWidth > 400)
      image.style.width = (currentWidth - 20) + 'px';
+     console.log(currentWidth);
 }
